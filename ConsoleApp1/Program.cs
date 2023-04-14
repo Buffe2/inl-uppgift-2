013f675c44402ec01e23c39b44de14b5a470618c
Admin <Admin@TEB127030>
2023-04-14 09:04:03 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Inl_uppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många heltal kommer du skriva?");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            int[] y = new int[x];
+
+            Console.WriteLine("Skriv in " + x + " heltal.");
+
+            for (int i = 0; i < x; i++)
+            {
+                y[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Du skrev talen i denna ordning:");
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine("Tal " + (i + 1) + ": " + y[i]);
+            }
+        }
+    }
+}
